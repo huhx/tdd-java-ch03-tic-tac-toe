c8f85c1bcc3f030bf37def04ded9cdf5b8feef75
huhx <gohuhx@163.com>
2019-06-17 15:57:42 +0800

    refactor the play method

--- src/main/java/com/linux/huhx/TicTacToe.java
+++ src/main/java/com/linux/huhx/TicTacToe.java
@@ -5,15 +5,22 @@ public class TicTacToe {
   private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
 
   public void play(int x, int y) {
-    if (x < 1 || x > 3) {
-      throw new RuntimeException("X is outside board");
-    } else if (y < 1 || y > 3) {
-      throw new RuntimeException("Y is outside board");
-    }
+    checkAxis(x);
+    checkAxis(y);
+    setBox(x, y);
+  }
+
+  private void setBox(int x, int y) {
     if (board[x - 1][y - 1] != '\0') {
       throw new RuntimeException("Box is occupied");
     } else {
       board[x - 1][y - 1] = 'X';
+    }
+  }
+
+  private void checkAxis(int x) {
+    if (x < 1 || x > 3) {
+      throw new RuntimeException("X is outside board");
     }
   }
 }
