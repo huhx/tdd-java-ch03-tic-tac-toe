8078fc2624b8c6d86b5adff7e6a562a63883cd2e
huhx <gohuhx@163.com>
2019-06-17 15:47:35 +0800

    init project and invalid placement of a piece

---
+++ src/main/java/com/linux/huhx/TicTacToe.java
@@ -0,0 +1,10 @@
+package com.linux.huhx;
+
+public class TicTacToe {
+
+  public void play(int x, int y) {
+    if (x < 1 || x > 3) {
+      throw new RuntimeException("X is outside board");
+    }
+  }
+}
